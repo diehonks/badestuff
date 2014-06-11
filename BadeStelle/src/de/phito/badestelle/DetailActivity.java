d0bf196ecc0d4b061f10035d58ad48be65e90d5a
twall <tomwallroth@gmail.com>
2014-06-11 18:33:17 +0200

    more clean-up, removed menu in detail activity

--- BadeStelle/src/de/phito/badestelle/DetailActivity.java
+++ BadeStelle/src/de/phito/badestelle/DetailActivity.java
@@ -2,10 +2,6 @@ package de.phito.badestelle;
 
 import java.util.Locale;
 
-import de.phito.badestelle.detailview.DescriptionFragment;
-import de.phito.badestelle.detailview.MapFragment;
-import de.phito.badestelle.model.BadeStelle;
-import de.phito.badestelle.model.BadeStellenContainer;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
@@ -14,11 +10,11 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import de.phito.badestelle.detailview.DescriptionFragment;
+import de.phito.badestelle.detailview.MapFragment;
+import de.phito.badestelle.model.BadeStelle;
+import de.phito.badestelle.model.BadeStellenContainer;
 
 public class DetailActivity extends FragmentActivity implements
 		ActionBar.TabListener {
@@ -87,13 +83,6 @@ public class DetailActivity extends FragmentActivity implements
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(this));
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 	@Override
